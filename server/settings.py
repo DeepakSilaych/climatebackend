02f7286a5cbf7770fe584c8f06ad8e816f21dc65
DeepakSilaych <deepaksilaych@gmail.com>
2024-06-09 15:45:06 +0530

    hoho

--- server/settings.py
+++ server/settings.py
@@ -112,8 +112,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/backend-static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/backend-media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
