c4803e59c43bc9d8d09cdb3caf86e3918a0052b2
DeepakSilaych <deepaksilaych@gmail.com>
2024-06-11 20:31:51 +0530

    o

--- awsstations/views.py
+++ awsstations/views.py
@@ -49,16 +49,27 @@ class StationDetailView(APIView):
 
         # add future timestamp to hourly_data
         pred_hrly = HourlyPrediction.objects.filter(station=station).latest('timestamp')
-        pred_hrly_data = {}
+        updated_hrly_data = []
+        for i in range(6):
+            updated_hrly_data.append({
+                'hour': str((now_time.hour - 6 + i)%24)+":00",
+                'total_rainfall': hourly_data[i]['total_rainfall']
+            })
         for i in range(24):
-            pred_hrly_data[str((now_time.hour + i)%24)+":00"] = pred_hrly.hr_24_rainfall[str(i)]
+            updated_hrly_data.append({
+                'hour': str((now_time.hour + i)%24)+":00",
+                'total_rainfall': pred_hrly.hr_24_rainfall[str(i)]
+            })  
         
         three_days_ago = now_time.date() - timedelta(days=3)
         daily_data_in_min = StationData.objects.filter(station=station, timestamp__gte=three_days_ago).order_by('-timestamp').values('timestamp', 'rainfall')
-        daily_data = daily_data_in_min.annotate(date=TruncDate('timestamp')).values('date').annotate(total_rainfall=Sum('rainfall')).order_by('date')[:3]
+        daily_data = daily_data_in_min.annotate(date=TruncDate('timestamp')).values('date').annotate(total_rainfall=Sum('rainfall')).order_by('date')
 
         pred_daily_data = DaywisePrediction.objects.filter(station=station).latest('timestamp')
-        pred_daily_data = {
+        updated_daily_data = {
+            str(daily_data[0]['date']): pred_daily_data.day1_rainfall,
+            str(daily_data[1]['date']): pred_daily_data.day2_rainfall,
+            str(daily_data[2]['date']): pred_daily_data.day3_rainfall,
             str(now_time.date() + timedelta(days=0)): pred_daily_data.day1_rainfall,
             str(now_time.date() + timedelta(days=1)): pred_daily_data.day2_rainfall,
             str(now_time.date() + timedelta(days=2)): pred_daily_data.day3_rainfall,
@@ -67,10 +78,7 @@ class StationDetailView(APIView):
         return Response({
             'station': serializer.data,
 
-            'obs_hrly_data': hourly_data,
-            'pred_hrly_data': pred_hrly_data,
-
-            'obs_daily_data': daily_data,
-            'pred_daily_data': pred_daily_data,
+            'hrly_data': updated_hrly_data,
+            'daily_data': updated_daily_data,
         })
 
