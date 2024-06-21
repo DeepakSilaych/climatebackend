e617d915bb9714b70064a3519b6a5a4f37ecd619
DeepakSilaych <deepaksilaych@gmail.com>
2024-06-21 16:09:24 +0530

    A

--- crowdsource/serializers.py
+++ crowdsource/serializers.py
@@ -4,7 +4,7 @@ from .models import CSFormData
 class CSFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = CSFormData
-        fields = ['latitude', 'longitude', 'feet', 'inch']
+        fields = ['latitude', 'longitude', 'feet', 'inch', 'location', 'timestamp']
 
 class FormDataSerializer(serializers.ModelSerializer):
     class Meta:
