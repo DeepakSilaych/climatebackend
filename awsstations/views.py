89652e6a0343bd80822439538f8a7da2a3d2b8e2
DeepakSilaych <deepaksilaych@gmail.com>
2024-07-23 16:40:28 +0530

    rag

--- awsstations/views.py
+++ awsstations/views.py
@@ -116,8 +116,9 @@ class StationDetailView(APIView):
 
         seasonaldata = []
         for data in stationdata:
+
             previous_day = data['date'] - timedelta(days=1)
-            predicted_rainfall = DaywisePrediction.objects.filter(station=station, timestamp__date=previous_day).first()
+            predicted_rainfall = DaywisePrediction.objects.filter(station=station, timestamp__date=previous_day).latest('timestamp').last()
             predicted_value = predicted_rainfall.day1_rainfall if predicted_rainfall else 0
             seasonaldata.append({
                 'date': data['date'],
